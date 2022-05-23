54aa8dd1bda9e7efb1f5d2decacb6ba2bb55794e
engrkashem <kashemaust@gmail.com>
2022-05-23 14:43:30 +0600

    user is saving to mongodb

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const jwt = require('jsonwebtoken');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -17,13 +18,27 @@ const run = async () => {
     try {
         await client.connect();
         const toolCollection = client.db('sks-inc').collection('tools');
+        const userCollection = client.db('sks-inc').collection('users');
 
         app.get('/tools', async (req, res) => {
-            const tools = await toolCollection.find().toArray();
+            const tools = await toolCollection.find().limit(6).toArray();
             res.send(tools);
         });
 
-
+        //create/update user or add user information to database
+        app.put('/user/:email', async (req, res) => {
+            const email = req.params.email;
+            const user = req.body;
+            console.log(email, user)
+            const filter = { email: email };
+            const option = { upsert: true };
+            const updatedUser = {
+                $set: user
+            };
+            const result = await userCollection.updateOne(filter, updatedUser, option);
+            const secretToken = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' });
+            res.send({ result, secretToken });
+        })
 
     }
     finally {
