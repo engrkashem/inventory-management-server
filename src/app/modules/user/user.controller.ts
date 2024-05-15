3d8ee95dab9b97e36b77b6b085fa7c146ec8320e
engrkashem <kashemaust@gmail.com>
2024-05-15 17:02:00 +0600

    send response function added to user create controller

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,11 +1,25 @@
+import config from '../../config';
 import catchAsyncRequest from '../../utils/catchAsyncRequest';
+import sendResponse from '../../utils/sendResponse';
 import { UserServices } from './user.service';
+
+const userRootUrl = `${config.BASE_URL}/users`;
 
 const createUser = catchAsyncRequest(async (req, res) => {
   const result = await UserServices.createUserIntoDB(req.body);
 
+  const links = {
+    self: `POST: ${userRootUrl}/create-user`,
+  };
+
   // sending response
-  res.json({ success: true, data: result });
+  sendResponse(res, {
+    status: 201,
+    success: true,
+    message: 'User is created successfully.',
+    data: result,
+    links,
+  });
 });
 
 export const UserControllers = {
